76a93cfd02e37eb2f7f2ef45a3519558657d9ca3
supriyaphalle <supriyaphalle@gmail.com>
2020-01-30 11:23:51 +0530

    MODIFIED: Removed if-else statements and Added case statements

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -6,19 +6,22 @@ isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
 
-randomCheck=$((RANDOM%3))
+#randomCheck=$((RANDOM%3))
 
-if (($isFullTime == $randomCheck ))
-then
-		empHrs=8;
-elif (( $isPartTime == $randomCheck ))
-then
-		empHrs=4;
-else
-		empHrs=0;
-fi
+case $((RANDOM%3)) in
+		$isFullTime)
+					empHrs=8
+				;;
+		$isPartTime)
+					empHrs=4
+				;;
+		*)
+					empHrs=0
+				;;
+		esac
 
-salary=$(( $empHrs * $empRatePerHr))#Constants
+
+salary=$(( $empHrs * $empRatePerHr))
 
 echo $salary
 
