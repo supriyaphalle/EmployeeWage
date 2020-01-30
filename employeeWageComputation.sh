134ab646cc12fe9fff6a04cdfeee97d1aff1a4b6
supriyaphalle <supriyaphalle@gmail.com>
2020-01-30 12:06:57 +0530

    MODIFIED:Removed for loop and added condition of working hrs and days for month

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -4,14 +4,20 @@ echo " Welcome to Employee Wage Computation Program"
 #Constants
 isPartTime=1;
 isFullTime=2;
-empRatePerHr=20;
+Max_HRS_IN_MONTH=100;
+EMP_RATE_PER_HR=20;
+Num_Working_Days=20;
 totalSalary=0;
 numWorkingDays=20;
 
+#VARIABLES
+TotalEmpHr=0;
+TotalWorkingDays=0
 
-for (( day=1; day<=$numWorkingDays; day++))
+
+while (( $TotalEmpHr < $Max_HRS_IN_MONTH && $TotalWorkingDays < $Num_Working_Days ))
 do
-
+	((TotalWorkingDays++))
 	case $((RANDOM%3)) in
 		$isFullTime)
 					empHrs=8
@@ -24,10 +30,9 @@ do
 				;;
 		esac
 
-
-	salary=$(( $empHrs * $empRatePerHr))
-	totalSalary=$(($totalSalary+$salary));
+	TotalEmpHr=$(($totalEmpHr+$empHrs))
 done
+	totalSalary=$(($TotalEmpHr*$EMP_RATE_PER_HR));
 
 echo $totalSalary
 
