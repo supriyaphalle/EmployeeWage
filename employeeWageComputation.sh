b824a04a417d04d16cf036e99eb356ccaf4c9dc3
supriyaphalle <supriyaphalle@gmail.com>
2020-01-30 12:27:40 +0530

    ADDED :Added work function to calculate daily working hrs

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -14,25 +14,28 @@ numWorkingDays=20;
 TotalEmpHr=0;
 TotalWorkingDays=0
 
+function getWorkingHours(){
+
+	case $1 in
+      $isFullTime)
+               empHrs=8
+            ;;
+      $isPartTime)
+               empHrs=4
+            ;;
+      *)
+               empHrs=0
+            ;;
+   esac
+	echo $empHrs
+}
 
 while (( $TotalEmpHr < $Max_HRS_IN_MONTH && $TotalWorkingDays < $Num_Working_Days ))
 do
 	((TotalWorkingDays++))
-	case $((RANDOM%3)) in
-		$isFullTime)
-					empHrs=8
-				;;
-		$isPartTime)
-					empHrs=4
-				;;
-		*)
-					empHrs=0
-				;;
-		esac
-
-	TotalEmpHr=$(($totalEmpHr+$empHrs))
+	workHours="$( getWorkingHours $((RANDOM%3)) )"
+	TotalEmpHr=$(($totalEmpHr+$workHours))
 done
-	totalSalary=$(($TotalEmpHr*$EMP_RATE_PER_HR));
 
+totalSalary=$(($TotalEmpHr*$EMP_RATE_PER_HR));
 echo $totalSalary
-
