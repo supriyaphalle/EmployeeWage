86aa25e726cadcaf771f36662d6817fce3c5b807
supriyaphalle <supriyaphalle@gmail.com>
2020-01-30 10:35:45 +0530

    ADDED: Added Employee is present or not

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 
 echo " Welcome to Employee Wage Computation Program"
+
+#Constants
+isAbsent=0
+isPresent=1;
+
+randomCheck=$((RANDOM%2))
+
+if (( $isAbsent == $randomCheck ))
+then
+		echo "Employee is Present "
+else
+		echo "Employee is absent"
+fi
