7174a41e55ee4c919dbe86c5ac6514dca9d53e59
supriyaphalle <supriyaphalle@gmail.com>
2020-01-30 10:58:30 +0530

    ADDED: Added the part time condition in Daily wage calculation

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -x
 
 echo " Welcome to Employee Wage Computation Program"
-
+#Constants
 isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
+
 randomCheck=$((RANDOM%3))
 
-if (( $isFullTime == $randomCheck ))
+if (($isFullTime == $randomCheck ))
 then
 		empHrs=8;
+elif (( $isPartTime == $randomCheck ))
+then
+		empHrs=4;
 else
 		empHrs=0;
 fi
