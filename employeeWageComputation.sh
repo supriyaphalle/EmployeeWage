a02bc0de1250ee55ca65a1fc15c6f51eec12aefd
supriyaphalle <supriyaphalle@gmail.com>
2020-01-30 12:55:25 +0530

    ADDED: Added Array To Store the Daily wage of Employee

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -9,35 +9,41 @@ isFullTime=2;
 Max_HRS_IN_MONTH=100;
 EMP_RATE_PER_HR=20;
 Num_Working_Days=20;
-totalSalary=0;
-numWorkingDays=20;
 
 #VARIABLES
-TotalEmpHr=0;
+TotalWorkHours=0;
 TotalWorkingDays=0
 
 function getWorkingHours(){
 
-	case $1 in
-      $isFullTime)
+		case $1 in
+   		  $isFullTime)
                empHrs=8
             ;;
-      $isPartTime)
+     		   $isPartTime)
                empHrs=4
             ;;
-      *)
+     			 *)
                empHrs=0
             ;;
-   esac
+  		 esac
 	echo $empHrs
 }
 
-while (( $TotalEmpHr < $Max_HRS_IN_MONTH && $TotalWorkingDays < $Num_Working_Days ))
+function calcDailyWage() {
+	local workHrs=$1
+	wage=$((workHrs*$EMP_RATE_PER_HR))
+	echo $wage
+}
+
+while (( $TotalWorkHours < $Max_HRS_IN_MONTH && $TotalWorkingDays < $Num_Working_Days ))
 do
 	((TotalWorkingDays++))
 	workHours="$( getWorkingHours $((RANDOM%3)) )"
-	TotalEmpHr=$(($totalEmpHr+$workHours))
+	totalWorkHours=$(($totalWorkHours+$workHours))
+	empDailyWage[$totalWorkingDays]="$( calcDailyWage $workHours )"
+
 done
 
-totalSalary=$(($TotalEmpHr*$EMP_RATE_PER_HR));
-echo $totalSalary
+totalSalary="$( calcDailyWage $TotalWorkHours )";
+echo "Daily Wage " ${empDailyWage[@]}
